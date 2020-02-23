1f2ea8f7cc2d1402f3a15dcfed3093cdafe280df
Allan Wanderley <awallan259@gmail.com>
2020-02-23 19:01:27 -0300

    added font family for typography using createMuiTheme

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import App from './App';
 
-//Redux imports
-import {createStore, combineReducers} from 'redux'
-import {Provider} from 'react-redux'
-import userReducer from './redux/userReducer'
-import errorReducer from './redux/errorReducer'
-import menuReducer from './redux/menuReducer'
-import toastReducer from './redux/toastReducer'
+// Redux imports
+import userReducer from './redux/userReducer';
+import errorReducer from './redux/errorReducer';
+import menuReducer from './redux/menuReducer';
+import toastReducer from './redux/toastReducer';
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-import './config/axios'
+import './config/axios';
 
-import './index.css'
+import './index.css';
 
 const reducers = combineReducers({
-    user: userReducer,
-    error: errorReducer,
-    menu: menuReducer,
-    toast: toastReducer
-})
+  user: userReducer,
+  error: errorReducer,
+  menu: menuReducer,
+  toast: toastReducer,
+});
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#088FF6'
-        },
-        secondary: {
-            main: '#8a05be'
-        },
-        action:{
-            disabled: 'rgb(0,0,0)',
-            disabledBackground: 'rgb(190,190,190)'
-        }
-    }
-})
-
+  palette: {
+    primary: {
+      main: '#088FF6',
+    },
+    secondary: {
+      main: '#8a05be',
+    },
+    action: {
+      disabled: 'rgb(0,0,0)',
+      disabledBackground: 'rgb(190,190,190)',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Quicksand',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <MuiThemeProvider theme={theme}>
-            <App />
-        </MuiThemeProvider>
-    </Provider>
-    , document.getElementById('root'));
+  <Provider store={createStore(reducers)}>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </Provider>,
+  document.getElementById('root'),
+);
