35693a0e23da3c2c72d3b5bf3ac9952b7a295dba
Allan Wanderley <awallan259@gmail.com>
2020-02-23 16:57:09 -0300

    changed scrolltotop function

--- src/config/ScrollToTop.js
+++ src/config/ScrollToTop.js
@@ -1,8 +1,5 @@
-import { withRouter } from "react-router";
+export function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
 
-const ScrollToTop = ({ children, location: { pathname } }) => {
-        window.scrollTo(0, 0);
-        return children || null;
-};
-
-export default withRouter(ScrollToTop);
+export default { scrollToTop };
