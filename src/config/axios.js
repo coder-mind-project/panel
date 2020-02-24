5b3c9d5d93c68b8c7ea89511f49b15094383e401
Allan Wanderley <awallan259@gmail.com>
2020-02-24 12:09:55 -0300

    refactored error function for axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,21 +1,26 @@
-import axios from 'axios'
-const success = resp => resp
-const error = async error => {
-    
-    const user = localStorage.getItem('user')
+import axios from 'axios';
 
-    if(error && error.response && error.response.status === 401 && user){
-        localStorage.removeItem('user')
-        window.location = '/'
-    }else if(error && error.response && error.response.status === 406 && user){
-        setTimeout(() => {
-            window.location = '/'
-        }, 3000)
-        return Promise.reject(error)
-    }else{
-        return Promise.reject(error)
+const success = (resp) => resp;
+const error = (err) => {
+  const user = localStorage.getItem('user');
+
+  if (err && err.response && user) {
+    const code = err.response.status;
+
+    if (code === 401) {
+      localStorage.removeItem('user');
+      window.location = '/';
     }
-}
+
+    if (code === 406) {
+      setTimeout(() => {
+        window.location = '/';
+      }, 3000);
+    }
+  }
+
+  return Promise.reject(error);
+};
 
 
-axios.interceptors.response.use(success, error)
+axios.interceptors.response.use(success, error);
