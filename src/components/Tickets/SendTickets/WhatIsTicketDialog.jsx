85cf90d0486f07a5fac0801812325031a838825d
Allan Wanderley <awallan259@gmail.com>
2020-02-28 18:14:01 -0300

    Refactored WhatIsTicketDialog component

---
+++ src/components/Tickets/SendTickets/WhatIsTicketDialog.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {
+  Grid,
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+  Typography,
+} from '@material-ui/core';
+
+
+function WhatIsTicketDialog(props) {
+  const { closeDialog } = { ...props };
+
+  return (
+    <Dialog
+      open
+      onClose={closeDialog}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        O que é ticket?
+      </DialogTitle>
+      <DialogContent>
+        <Grid item xs={12}>
+          <Box width="100%" display="flex" alignItems="center" flexDirection="column">
+            <Typography component="p" variant="body1">Ticket é a via de comunicação entre administradores e autores sobre quaisquer assuntos técnicos da plataforma.</Typography>
+            <Typography component="p" variant="body1">Todo problema relacionado a plataforma, sugestão de melhorias, reporte de bugs devem ser comunicados aqui.</Typography>
+            <Typography component="p" variant="body1">Informe o tipo de ticket e em seguida envie sua mensagem, você será respondido em breve!</Typography>
+          </Box>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={closeDialog} color="secondary" autoFocus>
+          Entendi
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default WhatIsTicketDialog;
