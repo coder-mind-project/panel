f03c57ee578ca1c48f2088bbdb23d90c77639105
Allan Wanderley <awallan259@gmail.com>
2020-03-04 17:15:48 -0300

    Added support to view anser user

--- src/components/Tickets/ManageTickets/TicketAnswer.jsx
+++ src/components/Tickets/ManageTickets/TicketAnswer.jsx
@@ -1,20 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Box,
   Typography,
+  Icon,
+  Menu,
+  MenuItem,
 } from '@material-ui/core';
 
 import { displayFullDate } from '@/config/masks';
 
-import { BoxMessageAdmin, BoxMessageIcon } from './styles';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { info } from '@/config/toasts';
+import { callToast as toastEmitter } from '@/redux/toastActions';
+
+import {
+  BoxMessageAdmin,
+  BoxMessageIcon,
+  BoxAnswerIdentifier,
+  BoxAnswerButton,
+} from './styles';
 
 function TicketAnswer(props) {
-  const { answer } = { ...props };
+  const { answer, callToast } = { ...props };
+  const sender = answer.adminId ? answer.admin : answer.user;
+
+  const [anchorMenu, setAnchorMenu] = useState(null);
+
+  function handleClick(event) {
+    const ref = event.currentTarget;
+    setAnchorMenu(ref);
+  }
+
+  function handleClose() {
+    setAnchorMenu(null);
+  }
+
+  function goToUserDetails() {
+    if (!sender || sender._id) callToast(info('Usuário não localizado'));
+    window.open(`/users/${sender._id}`, '_self');
+  }
 
   return (
     <BoxMessageAdmin fromadmin={answer.adminId || answer.pending}>
+      { sender
+        && (
+          <Box width="100%" display="flex" justifyContent="space-between" alignItems="center">
+            <Box display="flex" alignItems="center" overflow="hidden">
+              { sender.name.length >= 15
+                && (
+                <BoxAnswerIdentifier component="span" variant="body2">
+                  {sender.name || 'Usuário não autenticado'}
+                </BoxAnswerIdentifier>
+                )
+              }
+              { sender.name.length < 15
+                && (
+                  <Typography component="span" variant="body2">
+                    {sender.name || 'Usuário não autenticado'}
+                  </Typography>
+                )
+              }
+              &nbsp;&nbsp;
+              <BoxAnswerIdentifier component="span" variant="body2">
+                {sender.email}
+              </BoxAnswerIdentifier>
+            </Box>
+            <BoxAnswerButton onClick={handleClick}>
+              <Icon>
+                keyboard_arrow_down
+              </Icon>
+            </BoxAnswerButton>
+          </Box>
+        )
+      }
       <Box width="100%">
         <Typography component="p" variant="body1" align={answer.adminId || answer.pending ? 'right' : 'left'}>
           {answer.msg}
@@ -46,9 +107,32 @@ function TicketAnswer(props) {
           )
         }
       </Box>
+      <Menu
+        anchorEl={anchorMenu}
+        open={Boolean(anchorMenu)}
+        keepMounted
+        onClose={handleClose}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={goToUserDetails}>
+          <Typography component="span" variant="body1">
+            Abrir perfil do usuário
+          </Typography>
+        </MenuItem>
+      </Menu>
     </BoxMessageAdmin>
   );
 }
 
+const mapStateToProps = (state) => ({ toast: state.config });
+const mapDispatchToProps = (dispatch) => bindActionCreators({ callToast: toastEmitter }, dispatch);
 
-export default TicketAnswer;
+export default connect(mapStateToProps, mapDispatchToProps)(TicketAnswer);
