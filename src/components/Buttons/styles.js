eda326c8a22b1c6232557b1a129407b55fe3f45d
Allan Wanderley <awallan259@gmail.com>
2020-03-06 19:05:03 -0300

    Added responsivity

--- src/components/Buttons/styles.js
+++ src/components/Buttons/styles.js
@@ -1,11 +1,17 @@
 import { styled, Fab } from '@material-ui/core';
 
+import { devices } from '@/config/devices';
+
 export const CustomFab = styled(Fab)({
   zIndex: 1000,
   position: 'fixed',
   left: '90%',
   top: '80%',
   overflow: 'hidden',
+  [devices.mobileLarge]: {
+    left: '75%',
+    top: '75% ',
+  },
 });
 
 export default { CustomFab };
