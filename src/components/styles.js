3fddaa74f714e3b7db9548deefa0b80daf2af927
Allan Wanderley <awallan259@gmail.com>
2020-03-07 12:13:10 -0300

    Swaped Notfound.jsx styles to src/components in styled components

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,9 +1,23 @@
-import { styled } from '@material-ui/core';
+import { styled, Box } from '@material-ui/core';
 
 import { Link } from 'react-router-dom';
+import { devices } from '@/config/devices';
+
 
 export const CustomLink = styled(Link)({
   textDecoration: 'none',
 });
 
-export default { CustomLink };
+export const NotFoundBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '2rem',
+  [devices.tablet]: {
+    flexDirection: 'column',
+  },
+  '& img': {
+    maxWidth: '300px',
+  },
+});
