b6ef00812584d70dfd225746cd482a21eec298da
Allan Wanderley <awallan259@gmail.com>
2020-03-06 22:33:52 -0300

    Added styled components for Comments

---
+++ src/components/menu-application/Comments/styles.js
@@ -0,0 +1,16 @@
+import { styled, Menu, Icon } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+
+export const CustomLink = styled(Link)({
+  textDecoration: 'none',
+  color: '#444',
+});
+
+export const CustomIcon = styled(Icon)({
+  color: '#666',
+});
+
+export const CustomMenu = styled(Menu)({
+  marginTop: '.5rem',
+});
