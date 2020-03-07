a5d28c8dc19da8b2c892fa69c6b839cfb5e24b4f
Allan Wanderley <awallan259@gmail.com>
2020-03-07 00:16:22 -0300

    Added styled components to General components

---
+++ src/components/styles.js
@@ -0,0 +1,9 @@
+import { styled } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+
+export const CustomLink = styled(Link)({
+  textDecoration: 'none',
+});
+
+export default { CustomLink };
