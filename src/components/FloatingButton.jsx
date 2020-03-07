e50eea50610bfeb56c5039d7c2a1b14db98fca02
Allan Wanderley <awallan259@gmail.com>
2020-03-07 13:24:02 -0300

    Added Prop Types and refactored component

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Slide, Icon, useScrollTrigger,
 } from '@material-ui/core';
@@ -6,21 +7,46 @@ import {
 import { CustomFab } from '@/components/Buttons/styles';
 
 function FloatingButton(props) {
-  const { window, action, icon } = { ...props };
+  const {
+    window,
+    action,
+    icon,
+    color,
+    direction,
+  } = props;
 
   const params = { target: window ? window() : undefined, disableHysteresis: true };
   const trigger = useScrollTrigger(params);
 
   return trigger ? (
-    <Slide direction="up" in mountOnEnter unmountOnExit>
+    <Slide direction={direction} in mountOnEnter unmountOnExit>
       <CustomFab
-        color="primary"
+        color={color}
         onClick={action}
       >
-        <Icon>{icon || 'keyboard_arrow_up'}</Icon>
+        <Icon>{icon}</Icon>
       </CustomFab>
     </Slide>
   ) : null;
 }
 
+FloatingButton.propTypes = {
+  window: PropTypes.node,
+  action: PropTypes.func.isRequired,
+  icon: PropTypes.string,
+  color: PropTypes.oneOf([
+    'default',
+    'inherit',
+    'primary',
+    'secondary',
+  ]),
+  direction: PropTypes.string,
+};
+
+FloatingButton.defaultProps = {
+  window: null,
+  icon: 'keyboard_arrow_up',
+  color: 'primary',
+  direction: 'up',
+};
 export default FloatingButton;
