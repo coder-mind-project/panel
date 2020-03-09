abd275e7a0b9b15b1cc9fe2364b6e44af64ef06f
Allan Wanderley <awallan259@gmail.com>
2020-03-09 04:05:54 -0300

    Finished swap theme function

--- src/redux/theme/themeReducer.js
+++ src/redux/theme/themeReducer.js
@@ -1,9 +1,21 @@
-const INITIAL_STATE = false;
+/* eslint-disable no-param-reassign */
+const INITIAL_STATE = 'light';
 
 export default function (state = INITIAL_STATE, action) {
   if (action.theme) {
-    // eslint-disable-next-line no-param-reassign
+    const theme = {
+      type: action.theme === 'dark' ? 'dark' : 'light',
+    };
+
+    localStorage.setItem('coder-mind-app-theme', JSON.stringify(theme));
+
     state = action.theme;
+  } else {
+    const definedTheme = JSON.parse(localStorage.getItem('coder-mind-app-theme'));
+
+    if (definedTheme && definedTheme.type === 'dark') {
+      state = 'dark';
+    }
   }
 
 
