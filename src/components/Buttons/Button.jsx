5e7c1df3a8fed04f96145ba3e070849b03e2f084
Allan Wanderley <awallan259@gmail.com>
2020-03-09 20:28:29 -0300

    Removed hook use styles to add styles with styled compponent and refactored component

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Icon,
+  useMediaQuery,
+  CircularProgress,
+} from '@material-ui/core';
+
+import { devices } from '@/config/devices';
+import { CustomButton } from './styles';
+
+function Button(props) {
+  const {
+    color,
+    fullWidth,
+    variant,
+    onClick,
+    disabled,
+    disabledIcon,
+    loading,
+    icon,
+    iconSize,
+    circularProgressSize,
+    CircularProgressColor,
+    text,
+  } = props;
+
+  const matches = useMediaQuery(devices.tablet);
+
+  return (
+    <CustomButton
+      fullWidth={fullWidth || matches}
+      variant={variant}
+      color={color}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      { !disabledIcon && !loading
+        && (
+          <Icon
+            fontSize={iconSize}
+          >
+            {icon}
+          </Icon>
+        )
+      }
+      { loading
+        && (
+          <CircularProgress
+            size={circularProgressSize}
+            color={CircularProgressColor}
+          />
+        )
+      }
+      {text}
+    </CustomButton>
+  );
+}
+
+Button.propTypes = {
+  fullWidth: PropTypes.bool,
+  variant: PropTypes.oneOf([
+    'text',
+    'contained',
+    'outlined',
+  ]),
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  disabledIcon: PropTypes.bool,
+  loading: PropTypes.bool,
+  icon: PropTypes.string,
+  iconSize: PropTypes.oneOf([
+    'default',
+    'inherit',
+    'large',
+    'small',
+  ]),
+  circularProgressSize: PropTypes.number,
+  CircularProgressColor: PropTypes.oneOf([
+    'inherit',
+    'primary',
+    'secondary',
+  ]),
+  text: PropTypes.string,
+  color: PropTypes.oneOf([
+    'default',
+    'inherit',
+    'primary',
+    'secondary',
+  ]),
+};
+
+Button.defaultProps = {
+  fullWidth: false,
+  variant: 'contained',
+  disabled: false,
+  disabledIcon: false,
+  loading: false,
+  icon: '',
+  iconSize: 'default',
+  circularProgressSize: 15,
+  CircularProgressColor: 'inherit',
+  text: '',
+  color: 'primary',
+  onClick: null,
+};
+
+export default Button;
