98c58368ab84ca362f9df44a8ad746a87a326fd6
Allan Wanderley <awallan259@gmail.com>
2020-03-09 00:56:58 -0300

    Added conditional margin bottom and added height fixed

--- src/styles.js
+++ src/styles.js
@@ -3,9 +3,10 @@ import { styled, Box } from '@material-ui/core';
 import { devices } from './config/devices';
 
 export const AppContent = styled(Box)({
+  height: '100vh',
   marginTop: (props) => (props.user._id || props.isvalidating ? 80 : 0),
   marginLeft: (props) => (props.user._id || props.isvalidating ? 80 : 0),
-  marginBottom: 20,
+  marginBottom: (props) => (props.user._id || props.isvalidating ? 20 : 0),
   [devices.mobileLarge]: {
     marginLeft: '0 !important',
     marginBottom: '80px !important',
