555c345bf8b087604d20f539758c3bb9316d1439
Allan Wanderley <awallan259@gmail.com>
2020-03-09 04:04:13 -0300

    Finished swap theme function

--- src/config/themes.js
+++ src/config/themes.js
@@ -1,48 +1,27 @@
 import { createMuiTheme } from '@material-ui/core';
 
-export const standard = createMuiTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#8a05be',
+export function standard(theme) {
+  return createMuiTheme({
+    palette: {
+      type: theme,
+      primary: {
+        main: '#8a05be',
+      },
+      secondary: {
+        main: '#f44336',
+      },
+      action: {
+        disabled: 'rgb(0,0,0)',
+        disabledBackground: 'rgb(190,190,190)',
+      },
     },
-    secondary: {
-      main: '#f44336',
+    typography: {
+      fontFamily: [
+        'Quicksand',
+        'sans-serif',
+      ].join(','),
     },
-    action: {
-      disabled: 'rgb(0,0,0)',
-      disabledBackground: 'rgb(190,190,190)',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Quicksand',
-      'sans-serif',
-    ].join(','),
-  },
-});
+  });
+}
 
-/**
- * @description Not used, just simple template for add some themes.
- */
-export const anotherTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#8a05be',
-    },
-    secondary: {
-      main: '#f44336',
-    },
-    action: {
-      disabled: 'rgb(0,0,0)',
-      disabledBackground: 'rgb(190,190,190)',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Quicksand',
-      'sans-serif',
-    ].join(','),
-  },
-});
+export default { standard };
