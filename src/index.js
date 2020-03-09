8eb0bbdcb2f6359a46b77d0d89c4afa4e97f76d8
Allan Wanderley <awallan259@gmail.com>
2020-03-09 02:32:21 -0300

    Clean root file, splited redux store

--- src/index.js
+++ src/index.js
@@ -5,28 +5,16 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import { standard } from '@/config/themes';
 
 // Redux imports
-import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import userReducer from './redux/userReducer';
-import errorReducer from './redux/errorReducer';
-import menuReducer from './redux/menuReducer';
-import toastReducer from './redux/toastReducer';
+import store from './redux';
 
 import App from './App';
 
 import './config/axios';
-
 import './index.css';
 
-const reducers = combineReducers({
-  user: userReducer,
-  error: errorReducer,
-  menu: menuReducer,
-  toast: toastReducer,
-});
-
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <MuiThemeProvider theme={standard}>
       <App />
     </MuiThemeProvider>
