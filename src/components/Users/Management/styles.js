6c2a390945f9d5829b82dd6b811f8afa95b106cb
Allan Wanderley <awallan259@gmail.com>
2020-03-17 14:03:11 -0300

    Added styled 'UserForm' styled components

--- src/components/Users/Management/styles.js
+++ src/components/Users/Management/styles.js
@@ -4,15 +4,21 @@ import {
   Icon,
   TableCell,
   Container,
+  TextField,
+  Grid,
+  Paper,
+  Tooltip,
+  withStyles,
 } from '@material-ui/core';
 
 import { Link } from 'react-router-dom';
 import SearchBar from 'material-ui-search-bar';
 import { devices } from '@/config/devices';
 
+import { KeyboardDatePicker } from '@material-ui/pickers';
+
 export const HudLink = styled(Link)({
   textDecoration: 'none',
-  margin: '0 8px',
   [devices.tablet]: {
     width: '100%',
     margin: 0,
@@ -21,6 +27,7 @@ export const HudLink = styled(Link)({
 
 export const CustomLink = styled(Link)({
   textDecoration: 'none',
+  color: 'inherit',
 });
 
 export const HudButtons = styled(Box)({
@@ -30,6 +37,15 @@ export const HudButtons = styled(Box)({
   width: '60%',
   [devices.tablet]: {
     width: '100%',
+  },
+  '& a': {
+    margin: '0 8px',
+  },
+  '& > button': {
+    margin: '0 8px 10px 8px',
+    [devices.tablet]: {
+      width: '100%',
+    },
   },
 });
 
@@ -59,3 +75,63 @@ export const TableWrapper = styled(Container)({
     fontWeight: '400 !important',
   },
 });
+
+export const CustomTextField = styled(TextField)({
+  margin: '1rem',
+  minWidth: '300px',
+  [devices.tablet]: {
+    width: '100%',
+    minWidth: '100%',
+    margin: '1rem 0',
+  },
+});
+
+export const CustomKeyboardDatePicker = styled(KeyboardDatePicker)({
+  margin: '1rem',
+  minWidth: '300px',
+  [devices.tablet]: {
+    width: '100%',
+    minWidth: '100%',
+    margin: '1rem 0',
+  },
+});
+
+export const CustomIcon = styled(Icon)({
+  marginRight: '5px',
+});
+
+export const CustomGrid = styled(Grid)({
+  margin: '15px 0',
+});
+
+export const Form = styled(Paper)({
+  marginBottom: '20px',
+  paddingBottom: '20px',
+});
+
+export const FormHudButtons = styled(Box)({
+  display: 'flex',
+  alignItems: 'flex-start',
+  justifyContent: 'flex-end',
+  flexWrap: 'wrap',
+  width: '100%',
+  [devices.mobileLarge]: {
+    justifyContent: 'center',
+  },
+  '& button': {
+    margin: '0px 5px',
+    [devices.tablet]: {
+      margin: '5px 0px',
+    },
+  },
+});
+
+export const CustomTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 320,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
