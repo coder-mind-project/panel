c10bc150448fc22c84766a1d197711ed00c8e66c
Allan Wanderley <awallan259@gmail.com>
2020-03-19 16:31:05 -0300

    Created styled components - Created hud buttons components

---
+++ src/components/Themes/styles.js
@@ -0,0 +1,41 @@
+import {
+  styled,
+  Box,
+} from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+import SearchBar from 'material-ui-search-bar';
+import { devices } from '@/config/devices';
+
+export const HudLink = styled(Link)({
+  textDecoration: 'none',
+  [devices.tablet]: {
+    width: '100%',
+    margin: 0,
+  },
+});
+
+export const HudButtons = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  width: '60%',
+  [devices.tablet]: {
+    width: '100%',
+  },
+  '& a': {
+    margin: '0 8px',
+  },
+  '& > button': {
+    margin: '0 8px 10px 8px',
+    [devices.tablet]: {
+      width: '100%',
+    },
+  },
+});
+
+export const HudSearchBar = styled(SearchBar)({
+  [devices.tablet]: {
+    width: '100%',
+  },
+});
