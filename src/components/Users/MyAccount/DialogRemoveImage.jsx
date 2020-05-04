b3e04642cb2bfaa635b721b4165e3ed29264d26b
Allan Wanderley <awallan259@gmail.com>
2020-05-04 17:46:49 -0300

    Added support to dark theme

--- src/components/Users/MyAccount/DialogRemoveImage.jsx
+++ src/components/Users/MyAccount/DialogRemoveImage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { userType } from '@/types';
+import { userType, appTheme } from '@/types';
 
 import {
   Dialog,
@@ -27,6 +27,7 @@ function RemoveImage(props) {
     closeDialog,
     user,
     callToast,
+    theme,
   } = props;
 
   const [removing, setRemoving] = useState(false);
@@ -69,12 +70,15 @@ function RemoveImage(props) {
         <Button
           onClick={closeDialog}
           disabled={removing}
+          size="small"
         >
           Fechar
         </Button>
         <Button
           onClick={remove}
           color="primary"
+          size="small"
+          variant={theme === 'dark' ? 'contained' : 'text'}
           disabled={removing}
         >
           Confirmar
@@ -89,12 +93,13 @@ RemoveImage.propTypes = {
   closeDialog: PropTypes.func.isRequired,
   user: userType.isRequired,
   callToast: PropTypes.func.isRequired,
+  theme: appTheme.isRequired,
 };
 
 RemoveImage.defaultProps = {
   open: false,
 };
 
-const mapStateToProps = (state) => ({ toast: state.toast });
+const mapStateToProps = (state) => ({ toast: state.toast, theme: state.theme });
 const mapDispatchToProps = (dispatch) => bindActionCreators({ callToast: toastEmitter }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(RemoveImage);
