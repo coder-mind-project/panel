9fbabe7ed17363f3ac730524e0c789077292d449
Allan Wanderley <awallan259@gmail.com>
2020-07-11 22:10:10 -0300

    Updates: Version changed to 2.0.1

--- src/config/dataProperties.js
+++ src/config/dataProperties.js
@@ -42,7 +42,7 @@ export const LIMIT_LABEL = 'Limite: ';
 export const DISPLAYED_ROWS = (value) =>
   `${value.from}-${value.to} de ${value.count}`;
 
-export const APP_VERSION = '2.0.0';
+export const APP_VERSION = '2.0.1';
 export const APP_BUILD = 'Alpha';
 export const APP_DEPENDENCIES = 'https://github.com/coder-mind-project/panel/blob/master/package.json';
 export const APP_REPORT_US = 'https://github.com/coder-mind-project/coder-mind-panel/issues/new';
